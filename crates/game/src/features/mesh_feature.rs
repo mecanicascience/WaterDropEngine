817333064d8aece64b06e558c86bb0df12bc3427
xam4lor <xam4lor@gmail.com>
2024-09-09 12:21:30 +0200

    Cleanup of mesh renderer

--- crates/game/src/features/mesh_feature.rs
+++ crates/game/src/features/mesh_feature.rs
@@ -9,14 +9,14 @@ pub struct MeshFeature;
 impl Plugin for MeshFeature {
     fn build(&self, app: &mut App) {
         app
-            .add_systems(Startup, init_depth)
-            .add_systems(Update, resize_depth);
+            .add_systems(Startup, MeshDepthTexture::init_depth)
+            .add_systems(Update, MeshDepthTexture::resize_depth);
 
 
         app.get_sub_app_mut(RenderApp).unwrap()
-            .add_systems(Extract, (construct_pass, extract_depth_texture))
-            .add_systems(Render, build_ssbo_bind_group.in_set(RenderSet::BindGroups))
-            .add_systems(Render, render.in_set(RenderSet::Render))
+            .add_systems(Extract, (RenderMeshPass::construct_pass, MeshDepthTexture::extract_depth_texture))
+            .add_systems(Render, MeshPipeline::build_ssbo_bind_group.in_set(RenderSet::BindGroups))
+            .add_systems(Render, RenderMeshPass::render.in_set(RenderSet::Render))
             .init_resource::<MeshPipeline>();
     }
 
@@ -47,10 +47,10 @@ impl Plugin for MeshFeature {
 
 
 #[derive(Resource)]
-pub struct MeshPipeline {
-    pub index: CachedPipelineIndex,
-    pub ssbo_layout_built: WgpuBindGroupLayout,
-    pub ssbo_bind_group: Option<WgpuBindGroup>,
+struct MeshPipeline {
+    index: CachedPipelineIndex,
+    ssbo_layout_built: WgpuBindGroupLayout,
+    ssbo_bind_group: Option<WgpuBindGroup>,
 }
 impl FromWorld for MeshPipeline {
     fn from_world(world: &mut World) -> Self {
@@ -80,254 +80,273 @@ impl FromWorld for MeshPipeline {
         MeshPipeline { index: cached_index, ssbo_layout_built, ssbo_bind_group: None }
     }
 }
+impl MeshPipeline {
+    fn build_ssbo_bind_group(
+        render_instance: Res<WRenderInstance<'static>>, mut mesh_pipeline: ResMut<MeshPipeline>,
+        ssbo_buffer: Res<RenderAssets<GpuBuffer>>, render_mesh_pass: Res<RenderMeshPass>
+    ) {
+        // Check if the bind group is already created
+        if mesh_pipeline.ssbo_bind_group.is_some() {
+            return;
+        }
 
-fn build_ssbo_bind_group(
-    render_instance: Res<WRenderInstance<'static>>, mut mesh_pipeline: ResMut<MeshPipeline>,
-    ssbo_buffer: Res<RenderAssets<GpuBuffer>>, render_mesh_pass: Res<RenderMeshPass>
-) {
-    // Check if the bind group is already created
-    if mesh_pipeline.ssbo_bind_group.is_some() {
-        return;
+        // Get the ssbo buffer
+        let ssbo_buffer = match ssbo_buffer.get(&render_mesh_pass.ssbo_gpu) {
+            Some(buffer) => buffer,
+            None => return
+        };
+
+        // Create the bind group
+        let render_instance = render_instance.data.read().unwrap();
+        let bind_group = BindGroup::build("mesh_ssbo", &render_instance, &mesh_pipeline.ssbo_layout_built, &vec![
+            BindGroup::buffer(0, &ssbo_buffer.buffer)
+        ]);
+        mesh_pipeline.ssbo_bind_group = Some(bind_group);
     }
-
-    // Get the ssbo buffer
-    let ssbo_buffer = match ssbo_buffer.get(&render_mesh_pass.ssbo_gpu) {
-        Some(buffer) => buffer,
-        None => return
-    };
-
-    // Create the bind group
-    let render_instance = render_instance.data.read().unwrap();
-    let bind_group = BindGroup::build("mesh_ssbo", &render_instance, &mesh_pipeline.ssbo_layout_built, &vec![
-        BindGroup::buffer(0, &ssbo_buffer.buffer)
-    ]);
-    mesh_pipeline.ssbo_bind_group = Some(bind_group);
 }
+
 
 
 
 #[derive(Resource)]
-pub struct MeshDepthTexture {
-    pub texture: Handle<Texture>
+struct MeshDepthTexture {
+    texture: Handle<Texture>
 }
-
-fn init_depth(mut commands: Commands, server: Res<AssetServer>, window: Query<&Window>) {
-    let resolution = &window.single().resolution;
-    let texture = server.add(Texture {
-        label: "depth".to_string(),
-        size: (resolution.width() as usize, resolution.height() as usize, 1),
-        format: WTexture::DEPTH_FORMAT,
-        usages: TextureUsages::RENDER_ATTACHMENT | TextureUsages::TEXTURE_BINDING,
-        ..Default::default()
-    });
-    commands.insert_resource(MeshDepthTexture { texture });
-}
-
-fn resize_depth(mut commands: Commands, mut window_resized_events: EventReader<WindowResized>, server: Res<AssetServer>) {
-    for event in window_resized_events.read() {
-        // Recreate the depth texture
+impl MeshDepthTexture {
+    fn init_depth(mut commands: Commands, server: Res<AssetServer>, window: Query<&Window>) {
+        let resolution = &window.single().resolution;
         let texture = server.add(Texture {
             label: "depth".to_string(),
-            size: (event.width as usize, event.height as usize, 1),
+            size: (resolution.width() as usize, resolution.height() as usize, 1),
             format: WTexture::DEPTH_FORMAT,
             usages: TextureUsages::RENDER_ATTACHMENT | TextureUsages::TEXTURE_BINDING,
             ..Default::default()
         });
         commands.insert_resource(MeshDepthTexture { texture });
     }
+
+    fn resize_depth(mut commands: Commands, mut window_resized_events: EventReader<WindowResized>, server: Res<AssetServer>) {
+        for event in window_resized_events.read() {
+            // Recreate the depth texture
+            let texture = server.add(Texture {
+                label: "depth".to_string(),
+                size: (event.width as usize, event.height as usize, 1),
+                format: WTexture::DEPTH_FORMAT,
+                usages: TextureUsages::RENDER_ATTACHMENT | TextureUsages::TEXTURE_BINDING,
+                ..Default::default()
+            });
+            commands.insert_resource(MeshDepthTexture { texture });
+        }
+    }
+
+    fn extract_depth_texture(mut commands: Commands, depth_texture : ExtractWorld<Res<MeshDepthTexture>>) {
+        commands.insert_resource(MeshDepthTexture { texture: depth_texture.texture.clone() });
+    }
 }
 
-fn extract_depth_texture(mut commands: Commands, depth_texture : ExtractWorld<Res<MeshDepthTexture>>) {
-    commands.insert_resource(MeshDepthTexture { texture: depth_texture.texture.clone() });
-}
 
 
 
 
-pub struct RenderMeshBatch {
-    pub mesh: Handle<Mesh>,
-    pub first: usize,
-    pub count: usize,
+
+struct RenderMeshBatch {
+    mesh: Handle<Mesh>,
+    first: usize,
+    count: usize,
+    index_count: usize,
 }
 
 #[derive(Resource)]
-pub struct RenderMeshPass {
-    pub ssbo: Handle<Buffer>,
-    pub ssbo_gpu: Handle<Buffer>,
-    pub batches: Vec<RenderMeshBatch>,
+struct RenderMeshPass {
+    ssbo: Handle<Buffer>,
+    ssbo_gpu: Handle<Buffer>,
+    batches: Vec<RenderMeshBatch>,
 }
+impl RenderMeshPass {
+    fn construct_pass(
+        mut pass: ResMut<RenderMeshPass>, render_instance: Res<WRenderInstance<'static>>,
+        entities: ExtractWorld<Query<(&Transform, &Handle<Mesh>)>>, meshes: Res<RenderAssets<GpuMesh>>,
+        buffers: Res<RenderAssets<GpuBuffer>>
+    ) {
+        // Clear the batches of the previous frame
+        pass.batches.clear();
 
-fn construct_pass(
-    mut pass: ResMut<RenderMeshPass>, render_instance: Res<WRenderInstance<'static>>,
-    entities: ExtractWorld<Query<(&Transform, &Handle<Mesh>)>>, meshes: Res<RenderAssets<GpuMesh>>,
-    buffers: Res<RenderAssets<GpuBuffer>>
-) {
-    // Clear the batches of the previous frame
-    pass.batches.clear();
+        // Get the ssbo
+        let ssbo = match buffers.get(&pass.ssbo) {
+            Some(ssbo) => ssbo,
+            None => return
+        };
+        
+        // If no entities, return
+        if entities.is_empty() {
+            return
+        }
 
-    // Get the ssbo
-    let ssbo = match buffers.get(&pass.ssbo) {
-        Some(ssbo) => ssbo,
-        None => return
-    };
-    
-    // If no entities, return
-    if entities.is_empty() {
-        return
-    }
+        // Create the batches
+        let render_instance = render_instance.data.read().unwrap();
+        ssbo.buffer.map_write(&render_instance, |mut view| {
+            let mut first = 0;
+            let mut count = 1;
+            let mut last_mesh: Option<Handle<Mesh>> = None;
+            let data = view.as_mut_ptr() as *mut TransformUniform;
 
-    // Create the batches
-    let render_instance = render_instance.data.read().unwrap();
-    ssbo.buffer.map_write(&render_instance, |mut view| {
-        let mut first = 0;
-        let mut count = 1;
-        let mut last_mesh: Option<Handle<Mesh>> = None;
-        let data = view.as_mut_ptr() as *mut TransformUniform;
+            for (transform, mesh_handle) in entities.iter() {
+                // Check if new element in same batch
+                let last_mesh_ref = last_mesh.as_ref();
+                if last_mesh_ref.is_some() {
+                    if mesh_handle.id() == last_mesh_ref.unwrap().id() {
+                        // Update the ssbo
+                        let transform = TransformUniform::new(transform);
+                        unsafe {
+                            *data.add(first + count) = transform;
+                        }
 
-        for (transform, mesh_handle) in entities.iter() {
-            // Check if new element in same batch
-            let last_mesh_ref = last_mesh.as_ref();
-            if last_mesh_ref.is_some() {
-                if mesh_handle.id() == last_mesh_ref.unwrap().id() {
+                        // Increment the count
+                        count += 1;
+
+                        continue;
+                    } else {
+                        // Push the batch
+                        pass.batches.push(RenderMeshBatch {
+                            mesh: last_mesh_ref.unwrap().clone_weak(),
+                            first,
+                            count,
+                            index_count: match meshes.get(last_mesh_ref.unwrap()) {
+                                Some(mesh) => mesh.index_count as usize,
+                                None => 0
+                            }
+                        });
+
+                        // Reset the batch
+                        first += count;
+                        count = 1;
+                        last_mesh = None;
+                    }
+                }
+
+                // Update the last mesh and ssbo if loaded
+                if meshes.get(mesh_handle).is_some() {
+                    // Update the mesh
+                    last_mesh = Some(mesh_handle.clone_weak());
+
                     // Update the ssbo
                     let transform = TransformUniform::new(transform);
                     unsafe {
-                        *data.add(first + count) = transform;
+                        *data.add(first) = transform;
                     }
-
-                    // Increment the count
-                    count += 1;
-
-                    continue;
-                } else {
-                    // Push the batch
-                    pass.batches.push(RenderMeshBatch {
-                        mesh: last_mesh_ref.unwrap().clone_weak(),
-                        first,
-                        count
-                    });
-
-                    // Reset the batch
-                    first += count;
-                    count = 1;
-                    last_mesh = None;
                 }
             }
 
-            // Update the last mesh and ssbo if loaded
-            if meshes.get(mesh_handle).is_some() {
-                // Update the mesh
-                last_mesh = Some(mesh_handle.clone_weak());
-
-                // Update the ssbo
-                let transform = TransformUniform::new(transform);
-                unsafe {
-                    *data.add(first) = transform;
-                }
+            // Push the last batch
+            if let Some(last_mesh) = last_mesh {
+                pass.batches.push(RenderMeshBatch {
+                    mesh: last_mesh.clone_weak(),
+                    first,
+                    count,
+                    index_count: match meshes.get(&last_mesh) {
+                        Some(mesh) => mesh.index_count as usize,
+                        None => 0
+                    }
+                });
             }
-        }
+        });
 
-        // Push the last batch
-        if let Some(last_mesh) = last_mesh {
-            pass.batches.push(RenderMeshBatch {
-                mesh: last_mesh,
-                first,
-                count
-            });
-        }
-    });
-
-    // Update the ssbo
-    let ssbo_gpu = match buffers.get(&pass.ssbo_gpu) {
-        Some(ssbo) => ssbo,
-        None => return
-    };
-    ssbo_gpu.buffer.copy_from_buffer(&render_instance, &ssbo.buffer);
-}
-
-fn render(
-    (render_instance, swapchain_frame, pipeline_manager): (
-        Res<WRenderInstance<'static>>, Res<SwapchainFrame>,  Res<PipelineManager>
-    ),
-    camera_layout : Res<CameraFeatureRender>,
-    (meshes, textures): (
-        Res<RenderAssets<GpuMesh>>, Res<RenderAssets<GpuTexture>>
-    ),
-    (mesh_pipeline, render_mesh_pass, depth_texture): (
-        Res<MeshPipeline>, Res<RenderMeshPass>, Res<MeshDepthTexture>
-    )
-) {
-    // Get the render instance and swapchain frame
-    let render_instance = render_instance.data.read().unwrap();
-    let swapchain_frame = swapchain_frame.data.as_ref().unwrap();
-
-    // Check if depth texture is ready
-    let depth_texture = match textures.get(&depth_texture.texture) {
-        Some(tex) => if render_instance.surface_config.as_ref().unwrap().width == tex.texture.size.0
-            && render_instance.surface_config.as_ref().unwrap().height == tex.texture.size.1 {
-            tex
-        } else {
-            return
-        },
-        None => return
-    };
-
-    // Create the render pass
-    let mut command_buffer = WCommandBuffer::new(&render_instance, "mesh");
-    {
-        let mut render_pass = command_buffer.create_render_pass(
-            "mesh", &swapchain_frame.view,
-            Some(Operations {
-                load: LoadOp::Clear(Color { r: 0.0, g: 0.0, b: 0.0, a: 1.0 }),
-                store: StoreOp::Store,
-            }),
-            Some(&depth_texture.texture.view),
-        );
-
-        // Render the mesh
-        if let (
-            CachedPipelineStatus::Ok(pipeline),
-            Some(camera_bg),
-            Some(ssbo_bg)
-        ) = (
-            pipeline_manager.get_pipeline(mesh_pipeline.index),
-            &camera_layout.bind_group,
-            &mesh_pipeline.ssbo_bind_group
-        ) {
-            // Set the camera bind group
-            render_pass.set_bind_group(0, camera_bg);
-
-            // Set the pipeline
-            if render_pass.set_pipeline(pipeline).is_ok() {
-                // Set the ssbo
-                render_pass.set_bind_group(1, ssbo_bg);
-
-                for batch in render_mesh_pass.batches.iter() {
-                    // Get the mesh
-                    let mesh = match meshes.get(&batch.mesh) {
-                        Some(mesh) => mesh,
-                        None => continue
-                    };
-
-                    // Set the mesh buffers
-                    render_pass.set_vertex_buffer(0, &mesh.vertex_buffer);
-                    render_pass.set_index_buffer(&mesh.index_buffer);
-
-                    // Draw the mesh
-                    let instance_indices = batch.first as u32..((batch.first + batch.count) as u32);
-                    match render_pass.draw_indexed(0..mesh.index_count, instance_indices) {
-                        Ok(_) => {},
-                        Err(e) => {
-                            error!("Failed to draw: {:?}.", e);
-                        }
-                    };
-                }
-            } else {
-                error!("Failed to set pipeline.");
-            }
-        }
+        // Update the ssbo
+        let ssbo_gpu = match buffers.get(&pass.ssbo_gpu) {
+            Some(ssbo) => ssbo,
+            None => return
+        };
+        ssbo_gpu.buffer.copy_from_buffer(&render_instance, &ssbo.buffer);
     }
 
-    // Submit the command buffer
-    command_buffer.submit(&render_instance);
+    fn render(
+        (render_instance, swapchain_frame, pipeline_manager): (
+            Res<WRenderInstance<'static>>, Res<SwapchainFrame>,  Res<PipelineManager>
+        ),
+        camera_layout : Res<CameraFeatureRender>,
+        (meshes, textures): (
+            Res<RenderAssets<GpuMesh>>, Res<RenderAssets<GpuTexture>>
+        ),
+        (mesh_pipeline, render_mesh_pass, depth_texture): (
+            Res<MeshPipeline>, Res<RenderMeshPass>, Res<MeshDepthTexture>
+        )
+    ) {
+        // Get the render instance and swapchain frame
+        let render_instance = render_instance.data.read().unwrap();
+        let swapchain_frame = swapchain_frame.data.as_ref().unwrap();
+
+        // Check if depth texture is ready
+        let depth_texture = match textures.get(&depth_texture.texture) {
+            Some(tex) => if render_instance.surface_config.as_ref().unwrap().width == tex.texture.size.0
+                && render_instance.surface_config.as_ref().unwrap().height == tex.texture.size.1 {
+                tex
+            } else {
+                return
+            },
+            None => return
+        };
+
+        // Create the render pass
+        let mut command_buffer = WCommandBuffer::new(&render_instance, "mesh");
+        {
+            let mut render_pass = command_buffer.create_render_pass(
+                "mesh", &swapchain_frame.view,
+                Some(Operations {
+                    load: LoadOp::Clear(Color { r: 0.0, g: 0.0, b: 0.0, a: 1.0 }),
+                    store: StoreOp::Store,
+                }),
+                Some(&depth_texture.texture.view),
+            );
+
+            // Render the mesh
+            if let (
+                CachedPipelineStatus::Ok(pipeline),
+                Some(camera_bg),
+                Some(ssbo_bg)
+            ) = (
+                pipeline_manager.get_pipeline(mesh_pipeline.index),
+                &camera_layout.bind_group,
+                &mesh_pipeline.ssbo_bind_group
+            ) {
+                // Set the camera bind group
+                render_pass.set_bind_group(0, camera_bg);
+
+                // Set the pipeline
+                if render_pass.set_pipeline(pipeline).is_ok() {
+                    // Set the ssbo
+                    render_pass.set_bind_group(1, ssbo_bg);
+
+                    let mut old_mesh_id = None;
+                    for batch in render_mesh_pass.batches.iter() {
+                        // Set the mesh
+                        if old_mesh_id != Some(batch.mesh.id()) {
+                            let mesh = match meshes.get(&batch.mesh) {
+                                Some(mesh) => mesh,
+                                None => continue // Should not happen
+                            };
+
+                            // Set the mesh buffers
+                            render_pass.set_vertex_buffer(0, &mesh.vertex_buffer);
+                            render_pass.set_index_buffer(&mesh.index_buffer);
+                            old_mesh_id = Some(batch.mesh.id());
+                        }
+
+                        // Draw the mesh
+                        let instance_indices = batch.first as u32..((batch.first + batch.count) as u32);
+                        match render_pass.draw_indexed(0..batch.index_count as u32, instance_indices) {
+                            Ok(_) => {},
+                            Err(e) => {
+                                error!("Failed to draw: {:?}.", e);
+                            }
+                        };
+                    }
+                } else {
+                    error!("Failed to set pipeline.");
+                }
+            }
+        }
+
+        // Submit the command buffer
+        command_buffer.submit(&render_instance);
+    }
 }
